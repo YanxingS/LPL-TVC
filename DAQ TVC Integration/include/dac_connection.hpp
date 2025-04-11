80d7fc37c68e39a7f0a6cca5fec10e8ada41c425
YanxingS <yanxings@usc.edu>
2025-04-10 19:52:27 -0700

    new files

--- DAQ TVC Integration/include/dac_connection.hpp
+++ DAQ TVC Integration/include/dac_connection.hpp
@@ -5,36 +5,46 @@
 
 using namespace qindesign::network;
 
-enum states { BRAKE, CALIBRATE, VECTOR, ABORT };
+enum states { BRAKE, CALIBRATE, VECTOR };
 enum statuses { DISCONNECTED, CONNECTED };
+enum link {DISC, CONN};
 
 const IPAddress LOCALIP(192, 168, 0, 102);
 const uint8_t MAC[6] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 constexpr uint16_t PORT = 19690;
 
-const int COMMAND_LENGTH = 5;
+const int COMMAND_LENGTH = 6;
 
 class DAC_CONNECTION {
 private:
     int status;
     int state;
+    int linkState;
     
-    int message_length;
+    unsigned int message_length;
     char message[COMMAND_LENGTH];
     EthernetServer server;
     EthernetClient client;
 
+    void setState(int state); // Set a new state
+    void setStatus(int status); // Set a new status
+    void setLinkState(int linkState); // Set a new Link State
+
+
 public:
+    IPAddress ip; 
     DAC_CONNECTION();  // Constructor
     ~DAC_CONNECTION(); // Destructor (No Dynamic Memory Allocation)
 
     bool initialize(); // Establish MAC address and IP of Teensy
     bool connect();    // Wait for and establish a connection
     bool update();     // Check for messages and update state
+    void updateStatus(); // Update the connection status for the current client
+    void updateLinkState(); // Updates the current Link State
 
     int getState();    // Get the current state
     int getStatus();   // Get the current connection status
-    void setState(int state); // Set a new state
+    int getLinkState(); // Gets the current link state
 };
 
 #endif
--- DAQ TVC Integration/src/main.cpp
+++ DAQ TVC Integration/src/main.cpp
@@ -10,30 +10,50 @@ int tvc_state = BRAKE;
 
 void setup() {
   Serial.begin(115200); 
-  delay(2000);
+  while (!Serial) {};
+  Serial.println("STARTING...");
 
   if (dac.initialize()) {
     // Initialize the DAC Server
-    Serial.println("DAC INITIALIZATION SUCCESS!");
+    Serial.println("DAC INITIALIZATION SUCCESS! with IP");
+    Serial.println(dac.ip);
   }
   else {
     Serial.println("DAC INITIALIZAITON FAILED.");
     return;
   }
 
-  if (dac.connect()) {
-    Serial.println("CONNECTED!");
-  }
-  else {
-    Serial.println("CONNECTION FAILED.");
-  }
+  while(!dac.connect());
 
-  Serial.println("SKIP");
+  Serial.println("CONNECTED!");
 }
 
 void loop() {
-  if (dac.update()) {
-    // If a new message has come in from dac, update our current state. 
-    Serial.println(dac.getState());
+
+  // CHECK IF LINK STATE IS STILL ACTIVE 
+  dac.updateLinkState(); 
+
+  // IF LINK STATE IS NO LONGER ACTIVE, WAIT FOR INITIALIZATION + CONNECTION
+  if (dac.getStatus() == DISC) {
+    Serial.println("ETHERNET CABLE DISCONNECTED, WAITING FOR RECONNECT ...");
+    while (!dac.initialize());
+    Serial.println("ETHERNET CABLE RECONNECTED!");
   }
+
+  // Check if DAC CONNECTION IS STILL ACTIVE 
+  dac.updateStatus();
+
+  // If DAC CONNECTION IS NO LONGER ACTIVE, WAIT FOR RECONNECTION
+  if (dac.getStatus() == DISCONNECTED) {
+    Serial.println("DAC DISCONNECTED, WAITING FOR RECONNECT ... ");
+    while(!dac.connect());
+    Serial.println("DAC CONNECTED!");
+  }
+
+  // NOW THAT DAC CONNECTION HAS BEEN ESTABLISHED, CHECK FOR MESSAGES 
+  if (dac.getStatus() == CONNECTED) {
+    if (dac.update()) Serial.println(dac.getState());
+  }
+
+  // TVC CODE v
 }
